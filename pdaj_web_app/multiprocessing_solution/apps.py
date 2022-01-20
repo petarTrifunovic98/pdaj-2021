70d2dcc3c81a5859f2d57e177052ced7ccb45410
Petar <peki.trifunovic@gmail.com>
2022-01-20 18:21:24 +0100

    Created django web app

---
+++ pdaj_web_app/multiprocessing_solution/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MultiprocessingSolutionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'multiprocessing_solution'
---
+++ pdaj_web_app/sequential_solution/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SequentialSolutionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sequential_solution'
